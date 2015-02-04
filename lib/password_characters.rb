c6690901f34abce26af83b667253e420028c9ba2
David Celis <me@davidcel.is>
2015-02-03 19:31:13 -0800

    Revert "Revert "Revert "Use delegators #pennypinching"""
    This reverts commit 5f8d7dc10369e3e1dc3ad87ddb0e22828c10deea.

--- lib/password_characters.rb
+++ lib/password_characters.rb
@@ -1,18 +1,13 @@
-require 'forwardable'
 require 'set'
 
 class PasswordCharacters
-  extend Forwardable
-
-  def_delegator :@characters, :include?
-
   def initialize(characters)
     @characters = Set.new(characters)
   end
 
   def allowed?(password)
     password.split("").all? do |c|
-      self.include?(c)
+      @characters.include?(c)
     end
   end
 end
