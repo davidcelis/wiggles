a132f6badf98e1f8daefcfd4fe3c3737d9a771ba
David Celis <me@davidcel.is>
2015-02-03 18:20:44 -0800

    Use delegators #pennypinching
    Signed-off-by: David Celis <me@davidcel.is>

--- lib/password_characters.rb
+++ lib/password_characters.rb
@@ -1,13 +1,18 @@
+require 'forwardable'
 require 'set'
 
 class PasswordCharacters
+  extend Forwardable
+
+  def_delegator :@characters, :include?
+
   def initialize(characters)
     @characters = Set.new(characters)
   end
 
   def allowed?(password)
     password.split("").all? do |c|
-      @characters.include?(c)
+      self.include?(c)
     end
   end
 end
